66b606ae9cb57329bebd821955b9aa7e88a68650
Jose Hinojosa <74486457+SrTohru@users.noreply.github.com>
2025-02-26 23:51:04 -0700

    initial commit

---
+++ app/src/main/java/org/itson/edu/hinojoa/practica5/Product.kt
@@ -0,0 +1,7 @@
+package org.itson.edu.hinojoa.practica5
+
+data class Product(var name:String,
+                   var image: Int,
+                   var description: String,
+                   var price: Double
+)
